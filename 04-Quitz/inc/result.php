0d932766917f3a34793bfc295b8938aa8fe5f5b3
User <User@DESKTOP-QHG18FL>
2026-07-06 14:54:25 +0300

    strlen($user_answers)

--- 04-Quitz/inc/result.php
+++ 04-Quitz/inc/result.php
@@ -11,7 +11,7 @@
 	for($i = 0; $i < count($user_answers); $i++)
 	{
 		//$answer = explode('_', $user_answers[$i])[1];
-		$answer = $user_answers[$i][3];
+		$answer = $user_answers[$i][strlen($user_answers[$i])-1];
 		//echo $answer;
 		if($answer == $correct_answers[$i])$score++;
 	}
