a329d7dbadf818fc472e7789304ed8158919164e
User <User@DESKTOP-QHG18FL>
2026-07-20 15:30:18 +0300

    Cookie page

--- 06-CookiesAndSession/cookie.php
+++ 06-CookiesAndSession/cookie.php
@@ -9,7 +9,22 @@
 		{
 			setcookie('return', '1', time()+300);
 		}
-	echo '<h1>У этой страницы обязательно должна быть кодировка \'(Unicode UTF-8 without signature) - Codepage 65001\'</h1>';
-	echo $visitor ? 'Welcome back' : 'Hello';
-	echo '<img src="CODEPAGE.png">';
 ?>
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Cookies</title>
+</head>
+<body>
+    <h1>Cookies</h1>
+	<h1>У этой страницы обязательно должна быть кодировка <br>(Unicode UTF-8 without signature) - Codepage 65001</h1>
+	<h2>
+		<?=$visitor ? 'Welcome back' : 'Hello';?>
+	</h2>
+	<img src="CODEPAGE.png" style="width:1100px;height:600px;">
+
+	
+</body>
+</html>
