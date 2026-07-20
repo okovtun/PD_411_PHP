a4b726debf2a8cf1bfbb22c1a7435a569dbf97c3
User <User@DESKTOP-QHG18FL>
2026-07-20 16:26:23 +0300

    Drop cookies DONE

--- 06-CookiesAndSession/cookie.php
+++ 06-CookiesAndSession/cookie.php
@@ -9,6 +9,10 @@
 		{
 			setcookie('return', '1', time()+300);
 		}
+	#function ResetCookies()
+	#{
+	#		setcookie('return', '1', 0);
+	#}
 ?>
 <!DOCTYPE html>
 
@@ -20,11 +24,28 @@
 <body>
     <h1>Cookies</h1>
 	<h1>У этой страницы обязательно должна быть кодировка <br>(Unicode UTF-8 without signature) - Codepage 65001</h1>
+	<h1>При смене расширения имени файла кодировка сбрасывается, и ее обязательно нужно менять на<br>(Unicode UTF-8 without signature) - Codepage 65001</h1>
 	<h2>
 		<?=$visitor ? 'Welcome back' : 'Hello';?>
 	</h2>
+	<button onclick='ResetCookies()'>Сбросить</button>
 	<img src="CODEPAGE.png" style="width:1100px;height:600px;">
 
-	
+	<script>
+		function ResetCookies()
+		{
+			let request = new XMLHttpRequest();
+			request.onreadystatechange = function()
+			{
+				if (this.readyState == 4 && this.status == 200)
+				{
+					//TODO: вызвать функцию, которая стбосит печеньки
+					alert(this.responseText);
+				}
+			}
+			request.open("GET", "reset_cookies.php", true);
+			request.send();
+		}
+	</script>
 </body>
 </html>
